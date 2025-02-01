d0c35b7958d4da4bd7e23789026cf62bfb89daf1
Selva-Priya17 <selvapriya0017@gmail.com>
2025-02-02 02:22:40 +0530

    Admin codes

--- src/admin/AddProducts.jsx
+++ src/admin/AddProducts.jsx
@@ -1,11 +1,42 @@
 
 import React from 'react'
+import {Container, Row, Col, Form ,FormGroup} from "reactstrap";
+
 
 const AddProducts = () => {
   return (
-    <div>
-      hello add product
-    </div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12">
+          <h4>Add Product</h4>
+          <Form>
+             <FormGroup className="form_group">
+              <span>Product title</span>
+              <input type="text" placeholder="Double sofa" />
+             </FormGroup>
+
+             <FormGroup className="form_group">
+             <span>Short Description</span>
+             <input type="text" placeholder="lorem...."/>
+             </FormGroup>
+
+             <FormGroup className="form_group">
+             <span>Description</span>
+             <input type="text" placeholder="Description..."/>
+             </FormGroup>
+
+             <FormGroup className="form_group">
+             <span>Price</span>
+             <input type="number" placeholder="Rs.10000"/>
+             </FormGroup>
+
+             
+          </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,38 +1,78 @@
 import React from 'react'; 
-import { Container } from 'reactstrap';
-import useAuth from "../custom-hooks/useAuth";
-import "../style/admin-nav.css";
+import { Container, Row } from 'reactstrap'; 
+import useAuth from "../custom-hooks/useAuth"; 
+import "../style/admin-nav.css"; 
+import { NavLink } from "react-router-dom";
+
+const admin_nav = [
+  {
+    display: 'Dashboard',
+    path:'/dashboard'
+  },
+  {
+    display: 'Orders',
+    path:'/dashboard/orders'
+  },
+  {
+    display: 'Users',
+    path:'/dashboard/users'
+  },
+];
 
 const AdminNav = () => {
   const { currentUser } = useAuth();
 
   return (
-    <header className="admin_header">
-      <div className="admin_nav-top">
+    <>
+      <header className="admin_header">
+        <div className="admin_nav-top">
+          <Container>
+            <div className="admin_nav-wrapper-top">
+              <div className="logo">
+                <h2 className="name">Shrinath Furnitures</h2>
+              </div>
+
+              <div className="search_box">
+                <input type="text" placeholder="Search..." />
+                <span><i className="ri-search-2-line"></i></span>
+              </div>
+
+              <div className="admin_nav-top-right">
+                <span><i className="ri-notification-2-line"></i></span>
+                <span><i className="ri-settings-4-line"></i></span>
+                
+                {currentUser && currentUser.photoURL ? (
+                  <img src={currentUser.photoURL} alt="User" />
+                ) : (
+                  <span><i className="ri-user-line"></i></span>
+                )}
+              </div>
+            </div>
+          </Container>      
+        </div>
+      </header>
+
+      <section className="admin_menu" p-0>
         <Container>
-          <div className="admin_nav-wrapper-top">
-            <div className="logo">
-              <h2 className="name">Shrinath Furnitures</h2>
+          <Row>
+            <div className="admin_navigation">
+              <ul className="admin_menu-list">
+                {admin_nav.map((item, index) => (
+                  <li className="admin_menu-item" key={index}>
+                    <NavLink 
+                      to={item.path} 
+                      className={navClass => navClass.isActive ? "active__admin-menu" : ""}
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
             </div>
-
-            <div className="search_box">
-              <input type="text" placeholder="Search..." />
-              <span><i className="ri-search-2-line"></i></span>
-            </div>
-
-            <div className="admin_nav-top-right">
-              <span><i className="ri-notification-2-line"></i></span>
-              <span><i className="ri-settings-4-line"></i></span>
-              {currentUser && currentUser.photoURL ? (
-                <img src={currentUser.photoURL} alt="User" />
-              ) : (
-                <span className="default-avatar">A</span> // Fallback UI
-              )}
-            </div>
-          </div>
-        </Container>      
-      </div>
-    </header>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
 
